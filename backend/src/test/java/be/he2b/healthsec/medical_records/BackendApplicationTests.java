418d43c242743e80cb472d0dd2eef0af4c9f71a6
62294 <62294@etu.he2b.be>
2025-11-17 14:58:25 +0100

    setup(backend): initialize backend

---
+++ backend/src/test/java/be/he2b/healthsec/medical_records/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.he2b.healthsec.medical_records;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
